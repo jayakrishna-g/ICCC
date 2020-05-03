c9088a2f076412476b85abc6b212423254f33267
jayakrishna-g <gandhamallajayakrishna@hotmail.com>
2020-05-03 18:16:31 +0530

    updated Wrong Form

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,10 +8,12 @@ import { Router } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   fact;
+  dataloaded: Promise<boolean>;
   constructor(private data: DataService,public router: Router ) {
     data.getrandomfact().subscribe((res) => {
       const len = Math.floor(Math.random() * 1000);
       this.fact = res[len];
+      this.dataloaded = Promise.resolve(true);
     });
   }
 
