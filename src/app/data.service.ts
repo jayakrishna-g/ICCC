c1d062bdce98e293e8d451cda81912c4ed7a39b7
jayakrishna-g <jayakrishnagandhamalla@hotmail.com>
2021-03-28 20:53:19 +0530

    Prod mode

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-  url = 'http://localhost:3000/api';
-  constructor(private http: HttpClient) { }
+  url = 'https://bz-iccc.herokuapp.com/api';
+  constructor(private http: HttpClient) {}
   getratings(route) {
     return this.http.get(this.url + route);
   }
